3b5e8334ab67977c82da137215f8d9fa9fd3fe0e
parvatchatap <parvatichatap3@gmail.com>
2023-08-11 16:42:45 +0530

    first commit

---
+++ src/main/java/example/backend/backend/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package example.backend.backend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(Long id){
+        super("Could not found the user with id"+id);
+    }
+
+
+}
---
+++ src/main/java/example/backend/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package example.backend.backend.repository;
+
+import example.backend.backend.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
